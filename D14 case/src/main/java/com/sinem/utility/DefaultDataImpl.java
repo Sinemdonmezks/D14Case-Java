b5157562d57d500674de32d31a13efd4def6d856
Sinem Dönmez <101560776+Sinemdonmezks@users.noreply.github.com>
2023-01-13 15:26:02 +0300

    Update DataImpl

--- D14 case/src/main/java/com/sinem/utility/DefaultDataImpl.java
+++ D14 case/src/main/java/com/sinem/utility/DefaultDataImpl.java
@@ -19,12 +19,47 @@ private final HabitantService habitantService;
 
     private void saveImplToDatabase() {
         Habitant habitant1=Habitant.builder()
-                .nameSurname("John Doe")
+                .nameSurname("Rachel Green")
                 .isCitizen(true)
-                .id(100000L)
+                .id(100001L)
                 .hasDrivingLicense(true)
                 .build();
         habitantService.save(habitant1);
+         Habitant habitant2=Habitant.builder()
+                .nameSurname("Phoebe Buffay")
+                .isCitizen(false)
+                .id(100002L)
+                .hasDrivingLicense(true)
+                .build();
+        habitantService.save(habitant2);
+         Habitant habitant3=Habitant.builder()
+                .nameSurname("Chandler Bing")
+                .isCitizen(true)
+                .id(100003L)
+                .hasDrivingLicense(false)
+                .build();
+        habitantService.save(habitant3);
+         Habitant habitant4=Habitant.builder()
+                .nameSurname("Monica Geller")
+                .isCitizen(false)
+                .id(100004L)
+                .hasDrivingLicense(false)
+                .build();
+        habitantService.save(habitant4);
+         Habitant habitant5=Habitant.builder()
+                .nameSurname("Rose Galler")
+                .isCitizen(true)
+                .id(100005L)
+                .hasDrivingLicense(true)
+                .build();
+        habitantService.save(habitant5);
+         Habitant habitant6=Habitant.builder()
+                .nameSurname("Joey Tribbiani")
+                .isCitizen(true)
+                .id(100006L)
+                .hasDrivingLicense(true)
+                .build();
+        habitantService.save(habitant6);
     }
 
 }
